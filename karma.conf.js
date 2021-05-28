a9c2eb5820dd5c80fca02691f274a412ff5b4a56
nprestridge <nprestridge@users.noreply.github.com>
2021-05-27 21:20:40 -0400

    chore:  Remove karma-coverage-istanbul-reporter (deprecated).  Replace with karma-coverage.

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+// https://github.com/karma-runner/karma-coverage/blob/master/docs/configuration.md
 
 module.exports = function (config) {
     config.set({
@@ -9,24 +10,33 @@ module.exports = function (config) {
             require('karma-jasmine'),
             require('karma-chrome-launcher'),
             require('karma-jasmine-html-reporter'),
-            require('karma-coverage-istanbul-reporter'),
+            require('karma-coverage'),
             require('@angular-devkit/build-angular/plugins/karma'),
         ],
         client: {
             clearContext: false, // leave Jasmine Spec Runner output visible in browser
         },
-        coverageIstanbulReporter: {
-            dir: require('path').join(
-                __dirname,
-                './coverage/random-team-generator'
-            ),
-            reports: ['html', 'lcovonly', 'text-summary'],
+        coverageReporter: {
+            dir: 'coverage',
+            subdir: 'random-team-generator',
+            reporters: [
+                { type: 'html' },
+                { type: 'text-summary' }
+            ],
             fixWebpackSourcePaths: true,
-            thresholds: {
-                statements: 100,
-                lines: 100,
-                branches: 100,
-                functions: 100,
+            check: {
+                global: {
+                    statements: 100,
+                    lines: 100,
+                    branches: 100,
+                    functions: 100,
+                },
+                each: {
+                    statements: 100,
+                    lines: 100,
+                    branches: 100,
+                    functions: 100,
+                },
             },
         },
         reporters: ['progress', 'kjhtml'],
